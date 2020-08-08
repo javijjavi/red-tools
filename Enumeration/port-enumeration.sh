e655c268e2c53b6a4539a133617d18bed3367430
javijjavi <javigarcia118@gmail.com>
2020-08-08 22:17:30 +0200

    Port enumeration

--- Enumeration/port-enumeration.sh
+++ Enumeration/port-enumeration.sh
@@ -4,8 +4,9 @@ trap crtl_c INT
 
 function crtl_c(){
     echo -e "\n\n[*] Exiting...\n"
+    exit 0
 }
 
 for port in $(seq 1 65535); do
-    timeout 1 bash -c "echo '' < /dev/tcp/$1/$port" 2>/dev/null && "Port: $port - OPEN"
+    timeout 1 bash -c "echo '' < /dev/tcp/$1/$port" 2>/dev/null && "Port: $port -- OPEN" &
 done;wait
